19a200ddb84edb0b1f4e982ba42da76b7bc13bd7
Cesar B <cesar.bh87@gmail.com>
2024-10-22 15:17:31 -0600

    data driven excel2

--- src/test/java/webDriverMethods/DataDrivenTestCase.java
+++ src/test/java/webDriverMethods/DataDrivenTestCase.java
@@ -1,15 +1,18 @@
 package webDriverMethods;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DataDrivenTestCase {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -22,16 +25,54 @@ public class DataDrivenTestCase {
 		WebElement rate = driver.findElement(By.xpath("//input[@id='interest']"));
 		WebElement periodVal = driver.findElement(By.xpath("//input[@id='tenure']"));
 		WebElement period = driver.findElement(By.xpath("//select[@id='tenurePeriod']"));
+		Select dropPeriod = new Select(period);
+		
 		WebElement frequency = driver.findElement(By.xpath("//select[@id='frequency']"));
+		Select freq = new Select(frequency);
+		
 		WebElement calculateBtn = driver.findElement(By.xpath("//img[@src='https://images.moneycontrol.com/images/mf_revamp/btn_calcutate.gif']"));
 		WebElement clearBtn = driver.findElement(By.xpath("//img[@class='PL5']"));
 		
 		String filePath = System.getProperty("user.dir")+"\\testdata\\testData.xlsx";
 		
+		int lineas = ExcelUtils.getRowCount(filePath, "Sheet1");
+		int columnas = ExcelUtils.getCellCount(filePath, "Sheet1", lineas);
 		
+		System.out.println("Lineas="+ lineas);
+		System.out.println("Columnas="+columnas);
+		//System.out.println(ExcelUtils.getCellData(filePath, "Sheet1", 0,0 ));
 		
-		
-		
+		for(int i = 1; i<=1;i++) {
+			for(int j = 0; j<=4; j++) {
+				String stringData = ExcelUtils.getCellData(filePath,"Sheet1", i, j);
+				if(j==0) {
+					principal.sendKeys(stringData);					
+				}
+				if(j==1) {		
+			
+					rate.sendKeys(stringData);
+					
+				}
+				if(j==2) {
+					periodVal.sendKeys(stringData);
+				}
+				if(j==3) {												
+					dropPeriod.selectByVisibleText(stringData);					
+				}
+				if(j==4) {
+					freq.selectByVisibleText(stringData);
+				}
+				//if(j==5) {
+					
+				//}
+				calculateBtn.click();
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+				
+			//	WebElement result = driver.findElement(By.xpath("//strong[normalize-space()='24000.00']"));
+				
+				
+			}
+		}
 
 	}
 
